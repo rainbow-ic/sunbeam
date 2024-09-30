8a7adc7a2a1416edd46a06b0db1b036a01106a2f
Long Tran <57053685+baolongt@users.noreply.github.com>
2024-09-30 14:49:46 +0700

    Refactor constant imports and types for ICPSwap and KongSwap

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,6 +1,3 @@
 export const ICPSWAP_NODE_INDEX_CANISTER = "ggzvv-5qaaa-aaaag-qck7a-cai";
 
-export enum SupportedDEX {
-    ICPSwap,
-    Sonic,
-}
+export const KONGSWAP_BACKEND_CANISTER = "l4lgk-raaaa-aaaar-qahpq-cai";
