8a73366e1a9e9efa493eb711a759714f5ca8fa15
Long Tran <57053685+baolongt@users.noreply.github.com>
2024-10-11 08:45:41 +0700

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -7,20 +7,34 @@ export default {
     input: "src/index.ts", // Entry point of your library
     output: [
         {
-            file: "dist/index.cjs.js", // Output file for CommonJS
+            dir: "dist", // Output directory
             format: "cjs", // CommonJS format
             sourcemap: true, // Generate source maps
+            entryFileNames: "[name].cjs.js", // Output file name pattern
+            chunkFileNames: "[name]-[hash].cjs.js", // Chunk file name pattern
         },
         {
-            file: "dist/index.esm.js", // Output file for ES Module
+            dir: "dist", // Output directory
             format: "esm", // ES Module format
             sourcemap: true, // Generate source maps
+            entryFileNames: "[name].esm.js", // Output file name pattern
+            chunkFileNames: "[name]-[hash].esm.js", // Chunk file name pattern
         },
     ],
     plugins: [
         resolve(), // Resolve node_modules
         commonjs(), // Convert CommonJS modules to ES6
-        typescript(), // Compile TypeScript
+        typescript({
+            exclude: [
+                "**/__tests__",
+                "**/*.test.ts",
+                "coverage",
+                "dist",
+                "eslint.config.mjs",
+                "jest.config.js",
+                "rollup.config.js",
+            ], // Exclude files from compilation
+        }), // Compile TypeScript
         terser(), // Minify the output
     ],
 };
