8f47d3b49730b24f2e33a1729fe2ff6a5b903ece
Long Tran <57053685+baolongt@users.noreply.github.com>
2024-11-03 23:56:43 +0700

    Fix/publish script (#10)
    * restruct type folder
    
    * add get transaction method
    
    * 0.0.9-alpha-1
    
    * fix type issue
    
    * 0.0.9-alpha-2
    
    * fix call
    
    * 0.0.9-alpha-3
    
    * fix
    
    * 0.0.9-alpha-4
    
    * support common js
    
    * 0.0.9-alpha-5
    
    * update rollup
    
    * 0.0.9-alpha-6
    
    * update rollup
    
    * 0.0.9-alpha-7
    
    * pump version
    
    * 0.0.9-alpha-8
    
    * pump version
    
    * 0.0.9-alpha-9
    
    * update type
    
    * 0.0.10-alpha-10
    
    * change if to if else
    
    * 0.0.10-alpha-11
    
    * 0.0.9-alpha-11
    
    * update pool info
    
    * fix publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,6 +8,7 @@ if [ -z "$VERSION" ]; then
   echo "Error: Version is not specified."
   echo "Usage: bash publish.sh <version>"
   exit 1
+  set -eu -o pipefail
 fi
 
 # Run build and proceed only if successful
