46ab5d5a36aabd38d85d39b82319eb358894e3d7
Long Tran <57053685+baolongt@users.noreply.github.com>
2024-12-12 02:43:20 +0700

    fix kongswap candid

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from "./services";
-export * from "./types";
-export * from "./utils";
 export * from "./constant";
+export * from "./types";
+export * from "./services";
+export * from "./utils";
--- src/services/kongswap/KongSwap.ts
+++ src/services/kongswap/KongSwap.ts
@@ -218,6 +218,7 @@ export class KongSwap extends CanisterWrapper implements IDex {
         return poolsObj;
     }
     async getPool(token1: Token, token2: Token): Promise<KongSwapPool | null> {
+        console.log("getPool", token1, token2);
         if (token1.address === token2.address) {
             throw new Error("Tokens must be different");
         }
@@ -225,32 +226,53 @@ export class KongSwap extends CanisterWrapper implements IDex {
             throw new Error("Chain must be provided");
         }
 
-        const poolAddress = `${token1.chain}.${token1.address}_${token2.chain}.${token2.address}`;
+        const poolAddress = `${token1.address}`;
+
+        console.log("poolAddress", poolAddress);
 
         const poolsRes = await this.actor.pools([poolAddress]);
+
+        console.log("poolsRes", poolsRes);
         const result = parseResultResponse(poolsRes);
 
         if (result.pools.length === 0) {
-            throw new Error("Pool not found");
+            return null;
         }
 
-        const poolReply = result.pools[0];
+        for (const pool of result.pools) {
+            if (
+                (pool.address_0 === token1.address && pool.address_1 === token2.address) ||
+                (pool.address_0 === token2.address && pool.address_1 === token1.address)
+            ) {
+                const poolInfo: PoolInfo = {
+                    ...pool,
+                    address: `${pool.chain_0}.${pool.address_0}_${pool.chain_1}.${pool.address_1}`,
+                    token1: {
+                        symbol: pool.symbol_0,
+                        name: pool.address_0,
+                        address: `${pool.chain_0}.${pool.address_0}`,
+                        chain: pool.chain_0,
+                    },
+                    token2: {
+                        symbol: pool.symbol_1,
+                        name: pool.address_1,
+                        address: `${pool.chain_1}.${pool.address_1}`,
+                        chain: pool.chain_1,
+                    },
+                };
 
-        const poolInfo: PoolInfo = {
-            ...poolReply,
-            address: poolAddress,
-            token1,
-            token2,
-        };
+                return new KongSwapPool({ poolInfo, agent: this.agent, address: this.id });
+            }
+        }
 
-        return new KongSwapPool({ poolInfo, agent: this.agent, address: this.id });
+        return null;
     }
     async getPoolByAddress(address: string): Promise<KongSwapPool | null> {
         const poolsRes = await this.actor.pools([address]);
         const result = parseResultResponse(poolsRes);
 
         if (result.pools.length === 0) {
-            throw new Error("Pool not found");
+            return null;
         }
 
         const poolReply = result.pools[0];
